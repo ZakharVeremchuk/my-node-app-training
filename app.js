6e715d167430725850e1330037f4ab33892e7af3
Zakhar Veremchuk <zaharverem@gmail.com>
2026-02-15 15:27:48 +0200

    realize router and node endpoints

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const myRouter = require("./my-router");
+app.use('/my-router', myRouter);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+})
+
+app.all('/anything', (req, res, next) => {
+    console.log('Anything method.');
+    next();
+})
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
+
+// app.get('/contact', (req, res) => {
+//     res.send('<h1>Contact page</h1>');
+// });
+
+// app.get('/contact/:id', (req, res) => {
+//     res.send(`<h1>Contact</h1> Параметр: ${req.params.id}`)
+// });
+
+// app.patch('/user/:userid', (req, res) => {
+//     const id = req.params.userid
+// })
+
+// app.use((req, res, next) => {
+//     console.log('Наше проміжне ПЗ')
+//     next();
+// })
+
+// for urlencoded
+// app.use(express.urlencoded({extended: false}));
+
+// for json
+// app.use(express.json())
+// app.post('/login', (req, res, next) => {
+//     const { email, password } = req.body
+// })
+
+
